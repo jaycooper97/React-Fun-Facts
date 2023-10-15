780b316921e3abe1a0178bc1ba58ba6009b27242
jay cooper <15jacooper@gmail.com>
2023-10-15 01:29:23 -0400

    push

--- Fact.js
+++ Fact.js
@@ -7,7 +7,7 @@ class Fact extends Component {
             <div>
                 <img src={Mikasa} alt="Mikasa" className='comp-img'/>
                 <h3 className='par'>Mikasa's Lineage</h3>
-                <p className='par'>Did you know that Mikasa and Eren aren't actually related? Mikasa was actually adopted by Eren's father, Dr. Jaeger, after her mother's untimely death. They were considered to be high value, due to them being the last of their race, which made them prime targets of robbers and traffickers.  Mikasa's mother, unfortunately, was killed, and Mikasa was going to be sold into trafficking, but Dr. Jaeger saved her from such a grueling fate.</p>
+                <p className='par'>Did you know that Mikasa and Eren aren't actually related? Mikasa was actually adopted by Eren's father, Dr. Jaeger, after her mother's untimely death. They were considered to be high value, due to them being the last of their race, which made them prime targets of robbers and traffickers.  Mikasa's mother, unfortunately, was killed, and Mikasa was going to be sold into trafficking, but Dr. Jaeger saved her from such a gruelingand horrific fate.</p>
             </div>
         )
     }
